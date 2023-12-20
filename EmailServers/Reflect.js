769814b21bd61c94b1841cf0b101ef010ade26d4
jeevaphysicist <jeevaphysicist@gmail.com>
2023-12-20 10:19:14 +0530

    code submission

--- EmailServers/Reflect.js
+++ EmailServers/Reflect.js
@@ -15,12 +15,12 @@ router.post('/sendemail',(req,res)=>{
     let mailOptions = {
         from: process.env.email,
         to: "jeevaphysicist@gmail.com",
-        subject: `New Appointment from ${req.body.Username}`,
+        subject: `New Appointment from ${req.body.name}`,
         html: ` <h1>Contact Details</h1>
                 <div style="border: 2px solid; padding: 10px;">
-                <p>Name : ${req.body.Username}</p>
-                <p>Mobile no:${req.body.MobileNo}</p>
-                <p>Time Slot:${req.body.Timeslot}</p>
+                <p>Name : ${req.body.name}</p>
+                <p>Mobile no:${req.body.phoneNumber}</p>
+                <p>Time Slot:${req.body.timeSlot}</p>
                 </div>
               `
       };
